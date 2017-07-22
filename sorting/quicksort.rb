a2ab3f132993cea5cd3002addc9b79c36525b6b2
xortiz <xavier.ortiz.ch@gmail.com>
2017-07-22 17:22:21 -0400

    refactored quicksort

--- sorting/quicksort.rb
+++ sorting/quicksort.rb
@@ -1,22 +1,32 @@
 def quicksort(array)
   pivot = array.slice!(-1)
+
+  if pivot.nil? 
+    return []
+  end 
+
   right_array = []
   left_array = array
 
-  left_length = array.length
+  divide_array(left_array, right_array, pivot)
+
+  if left_array.length + right_array.length <= 1
+    return left_array + [pivot] + right_array
+  else
+    return quicksort(left_array) + [pivot] + quicksort(right_array)
+  end
+end
+
+def divide_array(left_array, right_array, pivot)
+  left_length = left_array.length
   (0...left_length).each do |step|
     location = (left_length - 1) - step
     if (left_array[location] >= pivot)
       right_array.push(left_array.slice!(location))
     end
   end
-
-  if left_array.length + right_array.length <= 1
-    return pivot.nil? ? left_array + right_array : left_array + [pivot] + right_array
-  else
-    return quicksort(left_array) + [pivot] + quicksort(right_array)
-  end
 end
-
+=begin
 puts quicksort([5, 4, 3, 2, 1]).to_s
 puts quicksort([-5, 5, 23, 42, 9, 4, 3, 200, 2, 1, -11]).to_s
+=end
