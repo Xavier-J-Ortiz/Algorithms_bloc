ad1a6ac635a59a11ec554ce033c84828e898c545
xortiz <xavier.ortiz.ch@gmail.com>
2017-07-29 11:58:16 -0400

    changed the fibonnaci variables to something better. :)

--- fibonnaci/fibonnaci.rb
+++ fibonnaci/fibonnaci.rb
@@ -1,22 +1,10 @@
 def fib n
-  fib_0 = 0
-  fib_1 = 1
+  fib_current = 0
+  fib_next = 1
   (0...n).each do | place |
-    fib_0_old = fib_0
-    fib_0 = fib_1
-    fib_1 = fib_0_old + fib_1
+    fib_previous = fib_current
+    fib_current = fib_next
+    fib_next = fib_previous + fib_next
   end
-  return fib_1
+  return fib_next
 end
-=begin
-puts fib(0)
-puts fib(1)
-puts fib(2)
-puts fib(3)
-puts fib(4)
-puts fib(5)
-puts fib(6)
-puts fib(7)
-puts fib(8)
-puts fib(9)
-=end
